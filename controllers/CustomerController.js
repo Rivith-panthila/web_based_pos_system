271f79b0dcefe962d41dcda349a2e3a5cad19c1a
Rivith Panthila <panthilarivith470@gmail.com>
2026-05-08 05:03:21 +0530

    updated

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -19,6 +19,16 @@ $('customerTable').on('click','.cust-edit-action',function(){
     let name=row.find('td').eq(1).text();
     let contact=row.find('td').eq(2).text();
     let address=row.find('td').eq(3).text();
+
+    $('#customerId').val(id);
+    $('#customerName').val(name);
+    $('#customerContact').val(contact);
+    $('#customerAddress').val(address);
+    
+    customerModalTitle.text("Edit Customer");
+    saveCustomerBtn.text("Update Customer");
+    $('#customerId').attr('readonly',true);
+    customermodal.show();
     
 });
 
@@ -41,7 +51,9 @@ saveCustomerBtn.on('click',function(){
 
     if(id!="" && name!="" && contact!="" && address!=""){
 
-        if(saveCustomerBtn.text())
+        if(saveCustomerBtn.text()=="save Customer"){
+            addCustomerData(id,name,contact,address);
+        }
     }
     
 })
