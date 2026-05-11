ce35dbcff14cb95a2e867290ebce8a1619c82428
Rivith Panthila <panthilarivith470@gmail.com>
2026-05-12 01:10:42 +0530

    update

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -3,3 +3,19 @@ import { getAllItems, updateItemStock } from "./ItemController.js"; // Stock upd
 import { saveOrderData, getAllOrders, generateNextOrderId } from "../model/OrderModel.js";
 
 let cart = [];
+function initializeOrderPage() {
+    // Fill Customer Dropdown
+    const customerSelect = $('#orderCustomer');
+    customerSelect.empty().append('<option value="">Choose a customer...</option>');
+    getAllCustomers().forEach(cust => {
+        customerSelect.append(`<option value="${cust.id}">${cust.id} - ${cust.name}</option>`);
+    });
+
+    const itemSelect = $('#orderItemSelect');
+    itemSelect.empty().append('<option value="">Choose an item...</option>');
+    getAllItems().forEach(item => {
+        itemSelect.append(`<option value="${item.code}">${item.name} (Qty: ${item.qty})</option>`);
+    });
+
+    loadAvailableItems();
+}
