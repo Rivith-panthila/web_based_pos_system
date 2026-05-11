9aeb263a982f0812fd0cb3c66056c19162127eae
Rivith Panthila <panthilarivith470@gmail.com>
2026-05-12 00:53:58 +0530

    order model created

--- db/db.js
+++ db/db.js
@@ -3,3 +3,6 @@ export {customer_db}
 
 const item_db=[];
 export {item_db}
+
+const order_db=[];
+export {order_db}
---
+++ model/OrderModel.js
@@ -0,0 +1,18 @@
+import { order_db } from '../db/db.js';
+
+export const order_db = [];
+
+class Order {
+    #orderId;
+    #customerId;
+    #date;
+    #items; // [{itemCode, name, qty, price, subtotal}]
+    #total;
+
+    constructor(orderId, customerId, date, items, total) {
+        this.#orderId = orderId;
+        this.#customerId = customerId;
+        this.#date = date;
+        this.#items = items;
+        this.#total = total;
+    }
