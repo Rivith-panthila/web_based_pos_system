ec611581bbc40f727e4d91f3bc9b92c4a2806c91
Rivith Panthila <panthilarivith470@gmail.com>
2026-05-14 16:09:04 +0530

    updated

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -2,5 +2,7 @@ import { customer_db } from '../db/db.js';
 import { item_db } from '../db/db.js';
 import { order_db } from '../db/db.js';
 
-
+export function updateDashboardStats() {
+    // Stat cards වල අගයන් update කිරීම
+  
 }
